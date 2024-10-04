07f570a2bc29ce54ac0c8de579df120f17148ea8
Aditya Kumar <adityacoding100@gmail.com>
2024-10-05 03:21:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ app = FastAPI()
 @app.get("/")
 def stock_data():
     import yfinance as yahooFinance
-    import json
+    
     # Here We are getting Facebook financial information
     # We need to pass FB as argument for that
     GetFacebookInformation = yahooFinance.Ticker("MCX.NS")
 
     # whole python dictionary is printed here
-    return (json.dumps(GetFacebookInformation.info))
+    return ((GetFacebookInformation.info))
